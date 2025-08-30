62622a102ae9704ccaf3994a5b99021742ac514b
dilanozer <dozer31415@gmail.com>
2025-08-30 17:48:06 +0300

    Identity Server

---
+++ src/IdentityService/Pages/Account/Register/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityService.Pages.Account.Register;
+
+public class RegisterViewModel
+{
+    [Required]
+    public string Email { get; set; }
+    [Required]
+    public string Password { get; set; }
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    public string FullName { get; set; }
+    public string ReturnUrl { get; set; } //send back the login page
+    public string Button { get; set; }
+}
