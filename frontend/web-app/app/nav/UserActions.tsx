adf10bb849164b56390cb371d1c7619ff555a202
dilanozer <dozer31415@gmail.com>
2025-09-14 20:05:50 +0300

    CRUD operations in the client side

--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -7,22 +7,40 @@ import React from 'react'
 import { AiFillCar, AiFillTrophy, AiOutlineLogout } from 'react-icons/ai';
 import Link from 'next/link';
 import { signOut } from 'next-auth/react';
+import { useParamsStore } from '@/hooks/useParamsStore';
+import { usePathname, useRouter } from 'next/navigation';
 
 type Props = {
   user: User
 }
 
 export default function UserActions({user}: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const setParams = useParamsStore(state => state.setParams);
+
+  function setWinner() {
+    setParams({winner: user.username, seller: undefined});
+    if (pathname !== '/') router.push('/');
+  }
+
+  function setSeller() {
+    setParams({seller: user.username, winner: undefined});
+    if (pathname !== '/') router.push('/');
+  }
+
   return (
     <Dropdown inline label={`Welcome ${user.name}`} className='cursor-pointer'>
-      <DropdownItem icon={HiUser}>
+      <DropdownItem icon={HiUser} onClick={setSeller}>
         My Auctions
       </DropdownItem>
-      <DropdownItem icon={AiFillTrophy}>
+      <DropdownItem icon={AiFillTrophy} onClick={setWinner}>
         Auctions won
       </DropdownItem>
       <DropdownItem icon={AiFillCar}>
-        Sell my car
+        <Link href='/auctions/create'>
+          Sell my car
+        </Link>
       </DropdownItem>
       <DropdownItem icon={HiCog}>
         <Link href='/session'>
